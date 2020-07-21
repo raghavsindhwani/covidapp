89085189790e5018775c4253822586b4d7f5d5be
raghavsindhwani <rsindhwani_be16@thapar.edu>
2020-07-21 15:16:53 +0530

    buld

---
+++ build/precache-manifest.9fb60961c353c492bf0687a4f30af73f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc7cc2555ececd92e679d897a010218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edd1d32167e64a599966",
+    "url": "/static/css/main.d445cbc4.chunk.css"
+  },
+  {
+    "revision": "52b349a1eccb1406d21c",
+    "url": "/static/js/2.087e0904.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.087e0904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd1d32167e64a599966",
+    "url": "/static/js/main.b62a4a23.chunk.js"
+  },
+  {
+    "revision": "d1098fafe75056363362",
+    "url": "/static/js/runtime-main.fdc060bd.js"
+  },
+  {
+    "revision": "a626e6abfcb1edb0501d99476714d2d1",
+    "url": "/static/media/ProximaNova-Black.a626e6ab.ttf"
+  },
+  {
+    "revision": "fc1744d0f66a10deb62c6bb2422ef8e9",
+    "url": "/static/media/ProximaNova-Bold.fc1744d0.ttf"
+  },
+  {
+    "revision": "85239849b40dcbd8af60fe11d5cdcc0b",
+    "url": "/static/media/ProximaNova-Light.85239849.ttf"
+  },
+  {
+    "revision": "ae31b7ac57b32b0d551f4d948abb43af",
+    "url": "/static/media/ProximaNova-Regular.ae31b7ac.ttf"
+  },
+  {
+    "revision": "0fed8f0d3fefa17a787dcbd0a6ba073b",
+    "url": "/static/media/ProximaNova-Semibold.0fed8f0d.ttf"
+  }
+]);
